0851f07f10cdad765a4f49cc9bc3dd9f58f67e70
Rohit <rohit-singh@pearlorganisation.com>
2025-10-07 18:31:46 +0530

    setup backend

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+// src/config/db.js
+import mongoose from "mongoose";
+ 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err.message);
+    throw err;
+  }
+};
+
+export default connectDB;
