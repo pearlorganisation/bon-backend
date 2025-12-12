b4a91c37b3a65af5fb9fe5061d97e09af1a91de9
ayushisinhapearl <ayushisinha@pearlorganisation.com>
2025-12-12 14:30:46 +0530

    add controllers in user

--- src/controllers/partner/property.controller.js
+++ src/controllers/partner/property.controller.js
@@ -267,10 +267,7 @@ export const addPropertyDetails = asyncHandler(async (req, res, next) => {
   );
 });
 
-
-
 export const getAllProperties = async (req, res) => {
-  
   try {
     const properties = await Property.find();
 
@@ -280,7 +277,6 @@ export const getAllProperties = async (req, res) => {
       "Properties fetched successfully",
       properties
     );
-
   } catch (error) {
     return res.status(500).json({
       success: false,
@@ -289,3 +285,37 @@ export const getAllProperties = async (req, res) => {
     });
   }
 };
+
+// ✅ Change Property Status (Active <--> Inactive) -- ADMIN ONLY
+export const changePropertyStatus = asyncHandler(async (req, res, next) => {
+  const adminId = req.user._id;
+  const role = req.user.role;
+
+  if (role !== "ADMIN") {
+    return next(new CustomError("Only admin can change property status", 403));
+  }
+
+  const propertyId = req.params.propertyId;
+  const { status } = req.body;
+
+  if (!["active", "inactive"].includes(status)) {
+    return next(
+      new CustomError("Status must be either 'active' or 'inactive'", 400)
+    );
+  }
+
+  const property = await Property.findById(propertyId);
+  if (!property) {
+    return next(new CustomError("Property not found", 404));
+  }
+
+  property.status = status;
+  await property.save();
+
+  successResponse(
+    res,
+    200,
+    `Property status updated to ${status} successfully`,
+    property
+  );
+});
--- src/models/Listing/property.model.js
+++ src/models/Listing/property.model.js
@@ -104,6 +104,18 @@ const propertySchema = new mongoose.Schema(
       },
     },
 
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
+      set: (val) => Math.round(val * 10) / 10,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+
     propertyApproval: {
       status: {
         type: String,
