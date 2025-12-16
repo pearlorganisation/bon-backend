f4b0918f034f55d1b725feb2892af4e9d1088292
rohit <rohit-singh@pearlorganisation.com>
2025-12-16 10:37:15 +0530

    .

--- src/controllers/DocumentRequest/documentRequest.controller.js
+++ src/controllers/DocumentRequest/documentRequest.controller.js
@@ -1,6 +1,7 @@
 import {
-  AdminDocument,
-  PropertyDocumentAccess,
+  Document,
+  PartnerDocumentAccess,
+  DocumentType
 } from "../../models/DocumentRequest/documentRequest.model.js";
 import Property from "../../models/Listing/property.model.js";
 
@@ -8,21 +9,28 @@ import successResponse from "../../utils/error/successResponse.js";
 import CustomError from "../../utils/error/customError.js";
 import asyncHandler from "../../middleware/asyncHandler.js";
 import { uploadFileToCloudinary } from "../../utils/cloudinary.js";
+import { isAdmin } from "../../middleware/auth/auth.middleware.js";
 
 // ==========================================
-// ADMIN CONTROLLERS
+// Documents CONTROLLERS
 // ==========================================s
 
-export const createMasterDocument = asyncHandler(async (req, res, next) => {
-  const { title, description, country, state, city, documentType } = req.body;
+export const createDocument = asyncHandler(async (req, res, next) => {
+  const { title, description, country, state, city, documentTypeId } = req.body;
+  const userId = req.user._id;
 
   if (!req.files || !req.files.document) {
     return next(new CustomError("Please upload a document file", 400));
   }
 
-  if (!documentType) {
+  if(!title || !country || !state ){
+     return next ( new CustomError(" title , country , state are required"));
+  }
+
+  if (!documentTypeId) {
     return next(new CustomError("Please specify the document type", 400));
   }
+
 
   const uploadResult = await uploadFileToCloudinary(
     req.files.document,
@@ -31,19 +39,178 @@ export const createMasterDocument = asyncHandler(async (req, res, next) => {
   const documentUrl = uploadResult[0].secure_url;
   const public_id = uploadResult[0].public_id;
 
-  const newDoc = await AdminDocument.create({
+   
+    const allreadyExist = await Document.findOne({
+      documentTypeId,
+      country: country.trim().toLowerCase(),
+      state: state.trim().toLowerCase(),
+    });
+
+    if(allreadyExist){
+      return next(
+        new CustomError(
+          "Document already exists for this country, state and document type",
+          409
+        )
+      );
+    }
+
+  const newDoc = await Document.create({
     title,
     description,
-    country: country.toLowerCase(),
-    state: state.toLowerCase(),
+    country: country.trim().toLowerCase(),
+    state: state.trim().toLowerCase(),
     city: city ? city.toLowerCase() : null,
-    documentType,
+    documentTypeId,
     documentUrl,
     public_id,
+    createdBy: userId,
   });
+ 
 
   successResponse(res, 201, "Master Document Created Successfully", newDoc);
 });
+
+export const updateDocument = asyncHandler(async (req, res, next) => {
+  const { id } = req.params; // documentId
+  const userId = req.user._id; // sub-admin or admin
+  const {
+    title,
+    description,
+    country,
+    state,
+    city,
+    documentTypeId,
+    isActive,
+  } = req.body;
+
+  // 1️⃣ Find document
+  const document = await Document.findOne({
+    _id: id,
+    isDeleted:false,
+  });
+
+  if (!document) {
+    return next(new CustomError("Document not found or soft deleted by admin", 404));
+  }
+
+  //ownership check  for sub admins
+  if (document.createdBy.toString() !== userId.toString() && req.user.role!="ADMIN") {
+    return next(new CustomError("Not allowed to update this document", 403));
+  }
+
+     //if new file uploaded 
+       if(req.files.document){
+        const uploadResult = await uploadFileToCloudinary(
+          req.files.document,
+          "admin/documents"
+        );
+        const documentUrl = uploadResult[0].secure_url;
+        const public_id = uploadResult[0].public_id;
+         document.documentUrl =documentUrl;
+         document.public_id= public_id;
+       }
+
+  // 2️⃣ Update only provided fields
+  if (title !== undefined) document.title = title;
+  if (description !== undefined) document.description = description;
+  if (country !== undefined) document.country = country.trim().toLowerCase();
+  if (state !== undefined) document.state = state.trim().toLowerCase();
+  if (city !== undefined) document.city = city.trim().toLowerCase();
+  if (documentTypeId !== undefined) document.documentTypeId = documentTypeId;
+
+  if(req.user.role==="Admin"){ //only admin can change  document status
+       if (isActive !== undefined) document.isActive = isActive;
+  }
+ 
+  try {
+    await document.save(); 
+
+    return res.status(200).json({
+      success: true,
+      message: "Document updated successfully",
+      data: document,
+    });
+  } catch (error) {
+    //  Handle UNIQUE index violation
+    if (error.code === 11000) {
+      return next(
+        new CustomError(
+          "Active document already exists for this country, state and document type",
+          409
+        )
+      );
+    }
+
+    throw error;
+  }
+});
+
+//only admin allow to soft delete document
+export const softDeleteDocument = asyncHandler(async (req, res, next) => {
+  const { id } = req.params; // documentId
+  const {isDeleted} =req.body;
+  // 1️⃣ Find document
+  const document = await Document.findById(id);
+
+  if (!document) {
+    return next(new CustomError("Document not found", 404));
+  }
+
+
+  // 3️⃣ If already deleted
+  if (!isDeleted) {
+    return next(new CustomError("delete value  required", 400));
+  }
+
+  // 4️⃣ Soft delete
+  document.isDeleted = isDeleted;
+  await document.save();
+
+  return res.status(200).json({
+    success: true,
+    message: `Document ${isDeleted?"soft deleted":"restore"} successfully`,
+  });
+});
+
+// for admin and sub-admins
+export const getDocuments =asyncHandler(async(req,res,next)=>{
+      
+  const userId  =  req.user._id;
+  
+   let response = {
+       isActive:[],
+       isInactive :[],
+       isDeleted :[]
+   }
+
+    let documents= [];
+
+    // check role 
+    if( req.user.role=== "ADMIN"){
+       documents = await Document.find().sort({ createdAt: 1 });
+    }
+    else{
+       documents =await  Document.find({createdBy:userId}).sort({createdAt:1});
+    }
+  
+       for( let doc of  documents){
+
+          if(doc.isDeleted){
+            response[isDeleted].push(doc);
+          }
+          else if(doc.isActive){
+           response[isActive].push(doc);
+          }
+          else{
+           response[isInctive].push(doc);
+          }
+       }
+   
+        return  successResponse(res,200,"documents fetch successfully",response);
+
+});
+
 
 export const getPendingDocRequests = asyncHandler(async (req, res, next) => {
   const requests = await PropertyDocumentAccess.find({ status: "pending" })
@@ -246,3 +413,97 @@ export const getMyPropertyDocuments = asyncHandler(async (req, res, next) => {
     pendingTypes: [...new Set(pendingTypes)], // Unique pending types
   });
 });
+
+
+
+// ==========================================
+//  Document Type
+// ==========================================
+
+
+export const createDocumentType= asyncHandler(async (req,res,next)=>{
+
+            const userId = req.user._id;
+
+            let {name , description} =req.body;
+
+            if(!name || !description ) {
+                return next(new CustomError("label and description required",400));
+            }
+
+             name  =name.trim().toLowerCase();
+
+            let alreadyExist= await DocumentType.findOne({label});
+
+            if(alreadyExist){
+              return next(new CustomError("Document Type Allready exist"));
+            }
+
+            const data = await DocumentType.create({
+              userId,
+              name,
+              description,
+              createdBy: userId // admin only
+            });
+
+            successResponse(res,201,"DocumentType Created Successfully",data);
+
+
+});
+
+export const updateDocumentType = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  let { name, description } = req.body;
+
+  const docType = await DocumentType.findById(id);
+
+  if (!docType) {
+    return next(new CustomError("Document type not found", 404));
+  }
+
+  if (name) {
+    name = name.trim().toLowerCase();
+
+    const exists = await DocumentType.findOne({
+      _id: { $ne: id },
+      name,
+    });
+
+    if (exists) {
+      return next(new CustomError("Document type name already exists", 409));
+    }
+
+    docType.name = name;
+  }
+
+  if (description) {
+    docType.description = description;
+  }
+
+  await docType.save();
+
+  successResponse(res, 200, "Document type updated successfully", docType);
+});
+
+export const deleteDocumentType = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+   const docType = await DocumentType.findByIdAndDelete(id);
+
+  if (!docType) {
+    return next(new CustomError("Document type not found", 404));
+  }
+
+
+  successResponse(res, 200, "Document type deleted successfully",docType);
+});
+
+
+export const getAllDocumentTypes = asyncHandler(async (req, res,next) => {
+
+  const docTypes = await DocumentType.find();
+   
+
+  successResponse(res, 200, "Document types fetched successfully", docTypes);
+});
+
--- src/routes/document.routes.js
+++ src/routes/document.routes.js
@@ -2,12 +2,7 @@ import express from "express";
 import multer from "multer";
 
 import {
-  createMasterDocument,
-  getPendingDocRequests,
-  grantDocumentAccess,
-  requestDocumentAccess,
-  getMyPropertyDocuments,
-  getAdminDocuments, // Import the new controller
+  // Import the new controller
 } from "../controllers/DocumentRequest/documentRequest.controller.js";
 import { protect, authorizeRoles } from "../middleware/auth/auth.middleware.js";
 
@@ -17,39 +12,39 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage });
 const uploadSingle = upload.fields([{ name: "document", maxCount: 1 }]);
 
-// PARTNER ROUTES
-router.post("/request-access", protect, requestDocumentAccess);
-router.get("/view-documents/:propertyId", protect, getMyPropertyDocuments);
+// // PARTNER ROUTES
+// router.post("/request-access", protect, requestDocumentAccess);
+// router.get("/view-documents/:propertyId", protect, getMyPropertyDocuments);
 
-// ADMIN ROUTES
-router.post(
-  "/admin/create-master-doc",
-  protect,
-  authorizeRoles("ADMIN"),
-  uploadSingle,
-  createMasterDocument
-);
+// // ADMIN ROUTES
+// router.post(
+//   "/admin/create-master-doc",
+//   protect,
+//   authorizeRoles("ADMIN"),
+//   uploadSingle,
+//   createMasterDocument
+// );
 
-// New: Get docs with filters (state/country) for the admin to select from
-router.get(
-  "/admin/documents",
-  protect,
-  authorizeRoles("ADMIN"),
-  getAdminDocuments
-);
+// // New: Get docs with filters (state/country) for the admin to select from
+// router.get(
+//   "/admin/documents",
+//   protect,
+//   authorizeRoles("ADMIN"),
+//   getAdminDocuments
+// );
 
-router.get(
-  "/admin/pending-requests",
-  protect,
-  authorizeRoles("ADMIN"),
-  getPendingDocRequests
-);
+// router.get(
+//   "/admin/pending-requests",
+//   protect,
+//   authorizeRoles("ADMIN"),
+//   getPendingDocRequests
+// );
 
-router.put(
-  "/admin/grant-access/:requestId",
-  protect,
-  authorizeRoles("ADMIN"),
-  grantDocumentAccess
-);
+// router.put(
+//   "/admin/grant-access/:requestId",
+//   protect,
+//   authorizeRoles("ADMIN"),
+//   grantDocumentAccess
+// );
 
 export default router;
