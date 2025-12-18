5f6822a348458f2671b20647e06c596f9fb7ce73
HimaniPearl <himanipanwar@pearlorganisation.com>
2025-12-18 15:26:44 +0530

    .

--- src/routes/partner/partner.route.js
+++ src/routes/partner/partner.route.js
@@ -20,9 +20,10 @@ import {
   setRoomImagesAndVideosById,
   setRoomsImagesandVideosInBulk,
   getRoomDetailsById,
+
   
 } from "../../controllers/partner/room.controller.js";
-import {partner_KYC,verify_property_GSTIN} from "../../controllers/partner/parnter.controller.js"
+import {partner_KYC,verify_property_GSTIN, getPartnerKYC} from "../../controllers/partner/parnter.controller.js"
 import { authorizeRoles, protect, optionalProtect } from "../../middleware/auth/auth.middleware.js";
 import multer from "multer";
 
@@ -52,6 +53,14 @@ router.post(
   protect,
   authorizeRoles("PARTNER"),
   verify_property_GSTIN
+);
+
+router.get(
+  "/verify-gstin",
+  protect,
+  authorizeRoles("PARTNER", "ADMIN"),
+  
+  getPartnerKYC
 );
 
 
