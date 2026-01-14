6ad36b44ee15b5b60323217c5963f3e349f5bc97
ayushisinhapearl <ayushisinha@pearlorganisation.com>
2026-01-14 14:43:33 +0530

    fix routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,15 +1,15 @@
 import express from "express";
 import multer from "multer";
 import { protect, isAdmin } from "../middleware/auth/auth.middleware.js";
+import { searchProperties } from "../controllers/partner/property.controller.js";
 import {
-  updateUserProfile,
-  getUserProfile,
-  getAllUsers,
-  getUserProfileById,
   deleteAllUsers,
+  getAllUsers,
+  getUserProfile,
+  getUserProfileById,
   updateAllUsers,
-  searchProperties
-} from "../controllers/partner/property.controller.js";
+  updateUserProfile,
+} from "../controllers/user.controller.js";
 
 const storage = multer.memoryStorage(); // or diskStorage
 const upload = multer({ storage });
@@ -31,6 +31,6 @@ router.delete("/users/:id", protect, isAdmin, deleteAllUsers);
 
 router.get("/profile/:userId", protect, getUserProfileById);
 
-router.get("/search",searchProperties);
+router.get("/search", searchProperties);
 
 export default router;
